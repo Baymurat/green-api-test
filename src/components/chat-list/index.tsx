1520f148df418aa634d2ebac86ad7677ce7d749f
Baymurat <baymurat.orazov@mail.ru>
2023-05-13 15:30:12 +0500

    feat: add minimal style to Chat list

--- src/components/chat-list/index.tsx
+++ src/components/chat-list/index.tsx
@@ -1,7 +1,7 @@
 import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-
+import Avatar from '@mui/material/Avatar'
 
 interface Chat {
   name: string
@@ -16,20 +16,35 @@ interface Props {
 
 const ChatList = ({ chats }: Props) => {
   return (
-    <Stack padding={'0.5rem 0.25rem'}>
+    <Stack
+      sx={{
+        bgcolor: '#fff',
+        height: '100%',
+        padding: '0.5rem 0.5rem',
+        overflowY: 'scroll'
+      }}
+    >
       {chats.map((chat) => (
         <Stack
           key={chat.name}
           direction={'row'}
+          py={1}
+          sx={{
+            borderBottom: '1px solid #e9edef',
+            '&:hover': {
+              cursor: 'pointer'
+            }
+          }}
         >
-          <Box flex={'0 1 auto'} pr={1} >
-            {chat.icon}</Box>
+          <Box flex={'0 1 auto'} pr={'1rem'} >
+            <Avatar alt={chat.name} src={chat.icon} />
+          </Box>
           <Stack flex={'1 1 auto'} >
             <Typography>{chat.name}</Typography>
-            <Typography variant='caption'>{chat.lastMessage}</Typography>
+            <Typography color={'GrayText'} variant='caption'>{chat.lastMessage}</Typography>
           </Stack>
           <Box flex={'0 1 auto'} >
-            <Typography variant='caption'>{chat.modifiedDate}</Typography>
+            <Typography color={'GrayText'} variant='caption'>{chat.modifiedDate}</Typography>
           </Box>
         </Stack>
       ))}
