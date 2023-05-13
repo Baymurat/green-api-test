a5719b9e718c3d21b56cdc3ab78adc422f34bf31
Baymurat <baymurat.orazov@mail.ru>
2023-05-13 14:03:01 +0500

    feat: add path alias @custom-types

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,7 @@ module.exports = {
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@context': path.resolve(__dirname, 'src/context'),
+      '@custom-types': path.resolve(__dirname, 'src/custom-types'),
     }
   }
 }
