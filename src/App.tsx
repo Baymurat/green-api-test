7efb9e9ed07d6c2e21c9c7766805607b424c17b4
Baymurat <baymurat.orazov@mail.ru>
2023-05-13 12:51:35 +0500

    feat: add whats app page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import LoginPage from '@pages/login';
 import PrivateRoutLayout from '@components/layout/PrivateRoutLayout';
 import { AuthContextProvier } from '@context/AuthContextProvider';
 import ErrorPage404 from '@pages/error404';
+import WhatsApp from '@pages/whats-app';
 
 function App() {
   return (
     <AuthContextProvier>
       <Router>
         <Routes>
-          <Route path='/' element={<PrivateRoutLayout />}>
-            <Route path='/whats-app' element={<>WHATS APP PAGE</>} />
+          <Route path='/'>
+            <Route path='/whats-app' element={<WhatsApp />} />
           </Route>
           <Route path='/login' element={<LoginPage />} />
           <Route path='*' element={<ErrorPage404 />} />
---
+++ src/pages/whats-app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const WhatsApp = (props: Props) => {
+  return (
+    <div>WhatsApp</div>
+  )
+}
+
+export default WhatsApp
