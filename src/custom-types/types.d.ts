05c42164691144d0f22a2324a0fd0c6dc75eee6e
Baymurat <baymurat.orazov@mail.ru>
2023-05-16 20:36:01 +0500

    feat: add new types

--- src/custom-types/types.d.ts
+++ src/custom-types/types.d.ts
@@ -19,6 +19,18 @@ export interface WSUser {
   apiTokenInstance: string
 }
 
+export interface IChatMessage {
+  type: 'outgoing' | 'incoming'
+  timestamp: number
+  idMessage: string
+  statusMessage?: 'pending' | 'sent' | 'delivered' | 'read'
+  typeMessage: TTypeMessage
+  chatId: string
+  textMessage: string
+  senderId?: string
+  senderName?: string
+}
+
 export interface Message {
   typeWebhook: TWebhook
   instanceData: IInstanceData
@@ -38,6 +50,16 @@ export type TWebhook =
   | "deviceInfo"                 /** уведомление об устройстве (телефоне) и уровне заряда батареи */
   | "incomingCall"               /** уведомление о входящем звонке */
 
+export type TTypeMessage =
+  | "textMessage"          /** текстовое сообщение */
+  | "imageMessage"         /** сообщение с изображением */
+  | "videoMessage"         /** видео сообщение */
+  | "documentMessage"      /** сообщение с файлом документа */
+  | "audioMessage"         /** аудио сообщение */
+  | "locationMessage"      /** сообщение геолокации */
+  | "contactMessage"       /** сообщение с контактом */
+  | "extendedTextMessage"  /** сообщение со ссылкой и превью */
+
 export interface IInstanceData {
   idInstance: integer
   wid: string
@@ -54,7 +76,7 @@ export interface ISenderData {
 export type TMessageData = TTextMessage
 
 interface TTextMessage {
-  typeMessage: 'textMessage'
+  typeMessage: Extract<TTypeMessage, 'textMessage'>
   textMessageData: ITextMessageData
   quotedMessage?: IQuotedMessage
 }
