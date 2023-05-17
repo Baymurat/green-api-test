2540de467b40d118c894c2323f49840cf10f5636
Baymurat <baymurat.orazov@mail.ru>
2023-05-17 18:20:18 +0500

    feat: enchance store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { messagesApi } from './features/messages/messagesSlice'
 import selectedChatSlice from './features/selectedChat/selectedChatSlice'
+import { notificationsMiddleware } from './features/notifications/middleware'
+import notificationsSlice from './features/notifications/notificationsSlice'
+import userSlice from './features/user/userSlice'
+import { chatListMiddleware } from './features/chat-list/middleware'
+import chatListSlice from './features/chat-list/chatListSlice'
 
 export const store = configureStore({
   reducer: {
     [messagesApi.reducerPath]: messagesApi.reducer,
-    selectedChatStore: selectedChatSlice.reducer
+    selectedChatStore: selectedChatSlice.reducer,
+    notificationsStore: notificationsSlice.reducer,
+    userStore: userSlice.reducer,
+    chatListStore: chatListSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(messagesApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .prepend(chatListMiddleware.middleware)
+    .concat(messagesApi.middleware)
+    .concat(notificationsMiddleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
